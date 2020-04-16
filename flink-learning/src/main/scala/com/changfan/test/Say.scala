212a45cbe030c9b5146446f1941d0d153d10d04f
sy <sy.@c.com>
2020-04-16 14:19:46 +0800

    a test for dev

---
+++ flink-learning/src/main/scala/com/changfan/test/Say.scala
@@ -0,0 +1,13 @@
+package com.changfan.test
+
+/**
+  * author: jiaozhu
+  * create: 2020-04-16-14:19
+  * tel: 17717876906
+  */
+object Say {
+  def main(args: Array[String]): Unit = {
+    println("test")
+  }
+
+}
