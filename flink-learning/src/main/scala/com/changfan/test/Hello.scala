461bc227f670ce7894d7f2da3da90942e148d89f
Songyang <ch.songyang@gmail.com>
2020-05-09 09:59:29 +0800

    测试

---
+++ flink-learning/src/main/scala/com/changfan/test/Hello.scala
@@ -0,0 +1,16 @@
+package com.changfan.test
+
+/**
+  * author: jiaozhu
+  * create: 2020-04-16-13:46
+  * tel: 17717876906
+  */
+object Hello {
+  def main(args: Array[String]): Unit = {
+//    println("hello ccccc")
+//    println("dev 不行 why")
+//    println("一个接一个错误")
+    print("shanchu")
+  }
+
+}
